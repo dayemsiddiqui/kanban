3c4376c1ec6bd80ac72cd2ee677976a8aa329013
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2019-10-04 02:15:25 +0500

    Firestore document fetch added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+
+const db = admin.firestore();
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine Built!');
+  let dataItems = [];
+  db.collection('tasks').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+      dataItems.push(doc.data())
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+  res.status(200).json({status: 'Success', payload: dataItems});
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
